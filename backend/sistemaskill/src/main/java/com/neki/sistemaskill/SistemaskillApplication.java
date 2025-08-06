db65919c8f251549ee2b401951b7324b25d45edf
Mateus Karl Peixoto <mateuskpeixoto@gmail.com>
2025-08-06 13:51:09 -0300

    Criação da base do projeto (Back)

---
+++ backend/sistemaskill/src/main/java/com/neki/sistemaskill/SistemaskillApplication.java
@@ -0,0 +1,13 @@
+package com.neki.sistemaskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaskillApplication.class, args);
+	}
+
+}
